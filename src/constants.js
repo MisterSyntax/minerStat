934856d525ad6477a3afcc91e7416d95f4ff88a7
MisterSyntax <asyriala@gmail.com>
2017-07-13 14:13:27 -0400

    Added constants.js to src

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+/**
+ * @desc: This File contains constains all of the constants for all of the actions in your project
+ * @TODO: remove the default action
+ */
+
+const constants = {
+    ACTION: "ACTION"
+}
+
+export default constants;
