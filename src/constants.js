ab8cfcd9dd201fdecdd850bf3f51bfac4aa26c65
MisterSyntax <asyriala@gmail.com>
2017-07-13 14:54:32 -0400

    added reducers.js

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,11 @@
 /**
  * @desc: This File contains constains all of the constants for all of the actions in your project
- * @TODO: remove the default action
+ * @TODO: remove the default ENABLE/DISABLE Actions
  */
 
 const constants = {
-    ACTION: "ACTION"
+    ENABLE: 'ENABLE',
+    DISABLE: 'DISABLE'
 }
 
 export default constants;
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,1 +1,23 @@
-import C from "../constants.js"
+/**
+ * @desc: Manage every key in the initialState.json file
+ * @return: A combined reducer that has the 
+ */
+
+import C from '../constants.js'
+import { combineReducers } from 'redux'
+
+//TODO: Remove/Replace Sample reducer
+export const enable = (state = false, action) => {
+    switch (action.type) {
+        case C.ENABLE: {
+            return true
+        }
+        case C.DISABLE: {
+            return false
+        }
+    }
+}
+
+export default combineReducers({
+    action
+})
