eca30c9e314a8c58ea80b79dd79e0b5ec6ed30f5
MisterSyntax <asyriala@gmail.com>
2017-07-14 15:25:20 -0400

    updated index.js comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 //React dependencies
 import React from 'react'
 import { render } from 'react-dom'
-
 //Redux dependencies
 import { Provider } from 'react-redux'
 import storeFactory from './store/'
-
 //sampleData
 import sampleData from './initialState.json'
-
 //Import Main Component
 import App from './components/App/'
 
+
 //Setting up load data and save state
 const initialState = (localStorage['app-store']) ? JSON.parse(localStorage['app-store']) : sampleData
-
 const saveState = () => 
     localStorage['app-store'] = JSON.stringify(store.getState())
-
 const store = storeFactory(initialState)
 store.subscribe(saveState)
-
-//For debuggin we expose the store
+//For debugging purposes we expose the store
 window.store = store
 
-//Renders your app
+
+//Renders your app, wrapped in a provider, for hooking up your store to the app
 render(
     <Provider store={store}>
         <App/>
