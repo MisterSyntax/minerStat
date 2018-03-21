8298a4d9974abcc38bddc896fafc1fcbae678321
Andrew Syriala <asyriala@overstock.com>
2018-03-20 23:32:42 -0600

    adds in some lightbox functionality. TODO: add store viewstate

--- src/App/LightBox/LightBox.js
+++ src/App/LightBox/LightBox.js
@@ -1,13 +1,28 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import styles from './lightbox.css';
 
-const LightBox = () => (
-    <Fragment>
-        <div id='light-box-bg'/>
-        <div id='light-box'>
-            CONTENT!
-        </div>
-    </Fragment>
-);
+const LightBox = (props) => {
+    if(props.hideOverlay) {
+        return null;
+    }
+
+    return (
+        <Fragment>
+            <div id={styles['light-box-bg']}/>
+            <div id={styles['light-box']}>
+                CONTENT!
+            </div>
+        </Fragment>);
+};
+
+LightBox.propTypes = {
+    hideOverlay: PropTypes.bool,
+};
+
+// TODO: Kill this once we have a selector to get the state of the of the LightBox
+LightBox.defaultProps = {
+    hideOverlay: true,
+};
 
 export default LightBox;
